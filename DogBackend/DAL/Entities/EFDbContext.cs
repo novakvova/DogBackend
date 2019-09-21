228b73092de62644283d2dd0d4c5ba1f1bb8cec8
novakvova <novakvova@gmail.com>
2019-09-21 10:15:15 +0300

    Add identity in base Asp.Net Core

---
+++ DogBackend/DAL/Entities/EFDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogBackend.DAL.Entities
+{
+    public class EFDbContext : IdentityDbContext<DbUser, DbRole, long, IdentityUserClaim<long>,
+        DbUserRole, IdentityUserLogin<long>,
+        IdentityRoleClaim<long>, IdentityUserToken<long>>
+    {
+        public EFDbContext(DbContextOptions<EFDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
--- DogBackend/Startup.cs
+++ DogBackend/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DogBackend.DAL.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +28,11 @@ namespace DogBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
+
+            services.AddDbContext<EFDbContext>(opt =>
+                opt.UseSqlServer(Configuration
+                    .GetConnectionString("DefaultConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
